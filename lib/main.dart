4fea4b050fb4923f5ee321a7072803d80ff7e4a3
Ayman <Aymanz.dev@gmail.com>
2021-05-25 06:49:22 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:molten_navigationbar_flutter/molten_navigationbar_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,6 +25,16 @@ class App extends StatelessWidget {
       appBar: AppBar(title: Text('Othman App')),
       body: Container(
         color: Colors.black87,
+      ),
+      bottomNavigationBar: MoltenBottomNavigationBar(
+        onTabChange: (index) {},
+        selectedIndex: 0,
+        tabs: [
+          MoltenTab(icon: Icon(Icons.add_alarm)),
+          MoltenTab(icon: Icon(Icons.access_alarms)),
+          MoltenTab(icon: Icon(Icons.delete_sweep)),
+          MoltenTab(icon: Icon(Icons.wine_bar)),
+        ],
       ),
     );
   }
