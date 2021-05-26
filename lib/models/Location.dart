67a039d1d015e87b902d836bb595fe83575b1fd5
FFSaharti <faisal.saharty@yahoo.com>
2021-05-26 09:06:21 +0300

    added new database asset

---
+++ lib/models/Location.dart
@@ -0,0 +1,14 @@
+class Location {
+  final int id;
+  final String name;
+
+  Location({this.id, this.name});
+
+  Location fromJson(Map json) {
+    if (json == null) return Location();
+    return new Location(
+      id: json['ID'],
+      name: json['name'],
+    );
+  }
+}
---
+++ lib/models/Verse.dart
@@ -0,0 +1,29 @@
+class Verse {
+  final int verseNumber;
+  final int suraNumber;
+  final int storyID;
+  final int locationID;
+
+  final String verse;
+  final String tafseer;
+
+  Verse(
+      {this.verseNumber,
+      this.suraNumber,
+      this.storyID,
+      this.locationID,
+      this.verse,
+      this.tafseer});
+
+  Verse fromJson(Map json) {
+    if (json == null) return Verse();
+    return new Verse(
+      verseNumber: json['verseNumber'],
+      suraNumber: json['suraNumber'],
+      storyID: json['storyID'],
+      locationID: json['locationID'],
+      verse: json['verse'],
+      tafseer: json['tafseer'],
+    );
+  }
+}
